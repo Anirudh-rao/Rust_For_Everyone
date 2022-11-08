807799fff5eb14a8c40b5321cd6438838f9023a3
Anirudh-rao <anirudh131299@gmail.com>
2022-11-08 22:18:45 +0530

    Added Some Changes

--- GuessingGame/src/main.rs
+++ GuessingGame/src/main.rs
@@ -1,15 +1,29 @@
 use std::io;
-
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Welcome To Our Rust Guessing Game");
     println!("Your Task  => Guess the Number");
     println!("Enter your Guess = -_-");
+    
+    //generating Secret Number
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("The Secret Number is :{}", secret_number);
 
-    let mut guess = String::new();
 
+
+    //Creating Guess
+    let  guess:u32 = guess.trim().parse().expect("Please type a Number");
     io::stdin().read_line(&mut guess).expect("Failed To Read Line");
-
     println!("You Guessed :{}", guess);
+
+
+    //Matching Guess in the Game
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => println!("You Win!"),
+    }
 
 }
