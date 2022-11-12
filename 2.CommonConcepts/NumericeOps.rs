93dcb1811f8f6a0e71e3c4520243d64b891e67a5
Anirudh-rao <anirudh131299@gmail.com>
2022-11-12 23:53:12 +0530

    Scalar and Numeric Operations

---
+++ 2.CommonConcepts/NumericeOps.rs
@@ -0,0 +1,23 @@
+fn main(){
+    //Addidion
+    let sum = 5+10;
+    println!("{}", sum);
+    
+    //Subtraction
+    let difference =  95.2  -4.3;
+    println!("{}", difference);
+    
+    //let multiplication 
+    let product = 4*30;
+    println!("{}", product);
+
+    //division
+    let quotient =  56.6 / 32.3;
+    println!("{}", quotient);
+
+    //remainder 
+    let remainder = 43 % 5;
+    println!("{}", remainder);
+
+
+}
---
+++ 2.CommonConcepts/ScalarTypes.rs
@@ -0,0 +1,16 @@
+ fn main(){
+    let x = 2.0; //f64
+
+    let  y: f32 =  3.0;  //f32
+
+    //Boolean
+
+    let t = true;
+    let f: bool = false; // with explicit type annotation
+ 
+   //Charecters
+   let c = 'z';
+   let z = 'Ƶ';
+   let heart_eyed_cat = '😻';
+    
+ }
