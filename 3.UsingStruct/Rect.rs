5184a1d5c7f4aa03fa60219cc4ad04852088a710
Anirudh-rao <anirudh131299@gmail.com>
2022-11-17 23:06:48 +0530

    Rectangle Program Using Rust

--- 3.UsingStruct/Rect.rs
+++ 3.UsingStruct/Rect.rs
@@ -5,7 +5,7 @@ fn main(){
     println!(
         "The Area of the Rectangle is {} Square Pixels"
         ,area(height,width)
-    )
+    );
 }
 fn area(width:u32, height:u32) -> u32{
     width*height
