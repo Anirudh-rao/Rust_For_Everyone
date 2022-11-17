dc2adc50ed1342b95607970bb9b703eeaa475cec
Anirudh-rao <anirudh131299@gmail.com>
2022-11-17 23:01:43 +0530

    Project Changes

--- 3.UsingStruct/Rect.rs
+++ 3.UsingStruct/Rect.rs
@@ -4,7 +4,7 @@ fn main(){
 
     println!(
         "The Area of the Rectangle is {} Square Pixels"
-        area(height,width);
+        ,area(height,width)
     )
 }
 fn area(width:u32, height:u32) -> u32{
